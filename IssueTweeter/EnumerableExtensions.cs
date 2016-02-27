519b6321607932b908a999b9b909a927ff1c72f9
Bar Arnon <i3arnon@gmail.com>
2016-02-27 04:33:37 +0200

    Add SelectManyAsync and run concurrently

--- IssueTweeter/EnumerableExtensions.cs
+++ IssueTweeter/EnumerableExtensions.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace IssueTweeter
 {
     public static class EnumerableExtensions
     {
-        public static async Task ForEachAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, Task> action)
-        {
-            foreach (var item in source)
-            {
-                await action(item);
-            }
-        }
+        public static Task ForEachAsync<TSource>(this IEnumerable<TSource> source, Func<TSource, Task> action) =>
+            Task.WhenAll(source.Select(action));
+
+        public static async Task<IReadOnlyCollection<TResult>> SelectManyAsync<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, Task<IReadOnlyCollection<TResult>>> projection) =>
+                (await Task.WhenAll(source.Select(projection))).
+                    SelectMany(_ => _).
+                    ToList();
     }
 }
