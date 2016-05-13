6f1244b3d6fef8472292f472c5fd65b7d7b76810
Bar Arnon <i3arnon@gmail.com>
2016-05-13 16:37:27 +0300

    update AssemblyInfo

--- IssueTweeter/Properties/AssemblyInfo.cs
+++ IssueTweeter/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("IssueTweeter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dave Glick")]
+[assembly: AssemblyCompany("Bar Arnon")]
 [assembly: AssemblyProduct("IssueTweeter")]
-[assembly: AssemblyCopyright("Copyright © Dave Glick")]
+[assembly: AssemblyCopyright("Copyright © Bar Arnon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
